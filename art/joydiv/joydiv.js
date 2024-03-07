1b1b977a27a80d24b910a85a00fb110fdf96da78
AthenasCode <emoore5203@gmail.com>
2024-03-07 17:50:29 +0800

    finished points explanation

--- art/joydiv/joydiv.js
+++ art/joydiv/joydiv.js
@@ -9,21 +9,31 @@ context.scale(dpr, dpr);
 context.lineWidth = 2;
 
 var step = 10;
-var lines = [];
 
-// Create the lines
+var lines = [];
+var startVariation = 50;
+
 for (var i = step; i <= size - step; i += step) {
   var line = [];
   for (var j = step; j <= size - step; j += step) {
-    var distanceToCenter = Math.abs(j - size / 2); // calc how far current vertical position is from center
-    var variance = Math.max(size / 2 - 50 - distanceToCenter, 0); // size/2 = center. 50 const. 0 means if first calc < 0, math.max chooses 0.
-    var random = (Math.random() * variance) / 2;
+    // calculate distance from center of canvas
+    var distanceToCenter = Math.abs(j - size / 2);
+
+    // generate multiplier based on how far from center (smaller distance = greater multiplier)
+    var multiplier = Math.max(size / 2 - startVariation - distanceToCenter, 0);
+
+    // generate random value multiplied by multiplier to create variation that trends bigger closer to center
+    var random = (Math.random() * multiplier) / 2;
     var point = { x: j, y: i - random };
     line.push(point);
   }
-  console.log("line", line); // e.g. [ {x: 10, y: 290} ... {x: 10, y: 268.2}, {x: 10, y: 283.5}, {x: 10, y: 240.23} ... {x: 10, y: 290} ]
+
   lines.push(line);
 }
+
+console.log(lines);
+// size/2 = center. 50 const. 0 means if first calc < 0, math.max chooses 0.
+// e.g. [ {x: 10, y: 290} ... {x: 10, y: 268.2}, {x: 10, y: 283.5}, {x: 10, y: 240.23} ... {x: 10, y: 290} ]
 
 // Draw lines, starting @ 5 to skip a few top lines
 for (var i = 5; i < lines.length; i++) {
